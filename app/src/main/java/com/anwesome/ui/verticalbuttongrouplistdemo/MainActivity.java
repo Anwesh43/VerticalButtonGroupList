3da8aa1553261ec55bcc0c8c289b06b85078ba57
Anwesh43 <anweshthecool0@gmail.com>
2017-05-27 14:33:03 +0530

    Using VerticalButtonGroupHolder in app

--- app/src/main/java/com/anwesome/ui/verticalbuttongrouplistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/verticalbuttongrouplistdemo/MainActivity.java
@@ -3,11 +3,17 @@ package com.anwesome.ui.verticalbuttongrouplistdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.verticalbuttongrouplist.VerticalButtonGroupHolder;
 
+public class MainActivity extends AppCompatActivity {
+    private String titles[] = {"Hello","Hert","Bye","Tick","MoreButtonMoreFun"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        VerticalButtonGroupHolder verticalButtonGroupHolder = new VerticalButtonGroupHolder(this);
+        for(int i=0;i<titles.length;i++) {
+            verticalButtonGroupHolder.addButton(titles[i]);
+        }
+        verticalButtonGroupHolder.show();
     }
 }
